53a75f1dfe63d3e8472b931fcda09c618b6471f9
Igor Belykh <igrbelykh@gmail.com>
2019-04-25 10:51:10 +0300

    реализовал стек

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,26 @@ public class App {
 
 
     public static void main(String[] args) {
+        Stack mStack = new Stack(5);
 
+        mStack.push(79);
+        mStack.push(59);
+        mStack.push(59);
+        mStack.push(59);
+
+
+//        mStack.deleteElement();
+        System.out.println(mStack.getSize());
+        System.out.print("Стек: ");
+        while (!mStack.isEmpty()) {
+            int value = mStack.pop();
+            System.out.print(value);
+            System.out.print(" ");
+
+        }
+        System.out.println("");
+        System.out.println(mStack.getSize());
     }
+}
     }
 
