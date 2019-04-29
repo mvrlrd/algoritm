349984889701e1fc2106d82b05752b5e2de8e216
Igor Belykh <igrbelykh@gmail.com>
2019-04-29 15:06:28 +0300

    реализован двухсторонний список

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,29 +2,49 @@ import java.util.Arrays;
 import java.util.Date;
 
 public class App {
-
+private static LinkedList linkedList = new LinkedList();
 
     public static void main(String[] args) {
-        Stack mStack = new Stack(5);
-
-        mStack.push(79);
-        mStack.push(59);
-        mStack.push(59);
-        mStack.push(59);
 
 
-//        mStack.deleteElement();
-        System.out.println(mStack.getSize());
-        System.out.print("Стек: ");
-        while (!mStack.isEmpty()) {
-            int value = mStack.pop();
-            System.out.print(value);
-            System.out.print(" ");
+        linkedList.insert("aa",12);
+        linkedList.insert("bb",14);
+        linkedList.insertLast("last",110);
+        linkedList.insert("cc",16);
+        
+        linkedList.display();
 
-        }
-        System.out.println("");
-        System.out.println(mStack.getSize());
-    }
+        linkedList.delete();
+        System.out.println();
+        linkedList.display();
+
+        System.out.println();
+        System.out.println(linkedList.find("bb"));
+
+        linkedList.delete("bb");
+        linkedList.display();
+
+//
+//        Stack mStack = new Stack(5);
+//
+//        mStack.push(79);
+//        mStack.push(59);
+//        mStack.push(59);
+//        mStack.push(59);
+//
+//
+////        mStack.deleteElement();
+//        System.out.println(mStack.getSize());
+//        System.out.print("Стек: ");
+//        while (!mStack.isEmpty()) {
+//            int value = mStack.pop();
+//            System.out.print(value);
+//            System.out.print(" ");
+//
+//        }
+//        System.out.println("");
+//        System.out.println(mStack.getSize());
+//    }
 }
     }
 
---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,78 @@
+class LinkedList{
+    private Link first;
+    private Link last;
+
+    public LinkedList(){
+        first = null;
+    }
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    public void insert(String name, int age){
+        Link newLink = new Link(name, age);
+        if (this.isEmpty())
+            last = newLink;
+        newLink.next = first;
+        first = newLink;
+    }
+
+
+    public void display()   {
+        Link current = first;
+        while(current != null)
+        {
+            current.display();
+            current = current.next;
+        }
+    }
+    public Link delete(){
+        Link temp = first;
+        if (first.next == null)
+            last = null;
+        first = first.next;
+        return temp;
+
+    }
+
+    public Link find(String name){
+        Link current = first;
+        while(current.name != name){
+            if(current.next == null)
+                return null;
+            else
+                current = current.next;
+        }
+        return current;
+    }
+
+    public Link delete(String name){
+        Link current = first;
+        Link previous = first;
+        while(current.name != name){
+            if(current.next == null)
+                return null;
+            else{
+                previous = current;
+                current = current.next;
+            }
+        }
+        if(current == first)
+            first = first.next;
+        else
+            previous.next = current.next;
+        return current;
+    }
+    public void insertLast(String name, int age){
+        Link newLink = new Link(name, age);
+        if (this.isEmpty()){
+            first = newLink;
+        } else {
+            last.next = newLink;
+        }
+        last = newLink;
+    }
+
+
+}
