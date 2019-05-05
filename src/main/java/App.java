72a9137d3b2dbac48c20c8ec6e8a59e2140fce5c
Igor Belykh <igrbelykh@gmail.com>
2019-05-05 11:49:55 +0300

    работает для 4,3,2 предметов

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+
+    private static double res=1;
+    private static int c=0;
+
+
+
     public static void main(String[] args) {
+    multiply(3,-6);
+
+//        System.out.println(res);
+
+
+       final Item item1 = new Item("a",10,11);
+        final Item item2 = new Item("b",1,1);
+        final Item item3 = new Item("c",13,20);
+        final Item item4 = new Item("d",29,60);
+
+        List<Item> list = new ArrayList<Item>(){{
+           add(item1);
+            add(item2);
+            add(item3);
+            add(item4);
+        }};
+//        System.out.println(list.size()+" size");
+//        for (int i = 0; i <list.size() ; i++) {
+//            System.out.println(list.get(i));
+//        }
+
+        Pack pack = new Pack(29);
+
+        pack.delete(list);
+//        System.out.println(
+//        pack.GetBestSet().toString());
+//        for (int i = 0; i <pack.GetBestSet().size() ; i++) {
+//            System.out.print( pack.GetBestSet().get(i).name);
+//            System.out.print("  ");
+
+//        }
+
+        System.out.println(pack.bestPrice);
+        for (int i = 0; i <pack.bestItems2.size() ; i++) {
+            System.out.print(pack.bestItems2.get(i).name+" ");
+        }
 
     }
+
+
+    private static double multiply(int x, int b){
+        if (b == 0) {
+            res = 1;
+            return res;
+        }
+        if (b == 1) {
+            return res=x;
+        }
+        if ((b>1)&&(b>c)) {
+            res = res * x;
+            c++;
+            multiply(x,b);
+        }
+        if ((b<0)&&(b<c)){
+            res=res/x;
+            c--;
+            multiply(x,b);
+        }
+        return  res;
+    }
+
+
+
 }
---
+++ src/main/java/Pack.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pack {
+
+
+    public static List<Item> bestItems2 = new ArrayList<Item>();
+
+    private static double maxW;
+    int n;
+
+    public double bestPrice;
+
+    public Pack(double _maxW) {
+        maxW = _maxW;
+    }
+
+
+    //вычисляет общий вес набора предметов
+    private double CalcWeigth(List<Item> items) {
+        double sumW = 0;
+        for (int i = 0; i < items.size() ; i++) {
+            sumW += items.get(i).weight;
+        }
+
+        return sumW;
+    }
+
+    //вычисляет общую стоимость набора предметов
+    private double CalcPrice(List<Item> items) {
+        double sumPrice = 0;
+        for (int i = 0; i < items.size() ; i++) {
+            sumPrice += items.get(i).price;
+        }
+        return sumPrice;
+    }
+
+    private void CheckSet(List<Item> items) {
+
+            if(CalcWeigth(items) <= maxW && CalcPrice(items) > bestPrice)
+            {
+
+
+                for (int i = 0; i < items.size() ; i++) {
+                    if (!bestItems2.contains(items.get(i))) {
+                        bestItems2.add(items.get(i));
+                    }
+                    for (int j = 0; j <bestItems2.size() ; j++) {
+                        if (!items.contains(bestItems2.get(i))){
+                            bestItems2.remove(i);
+                        }
+                    }
+
+
+                }
+//                bestItems = items;
+                bestPrice = CalcPrice(items);
+            }
+//        }
+    }
+
+
+
+
+
+
+//    }
+
+    public void delete(List<Item> items){
+            CheckSet(items);
+        if (items.size()==0){
+            return;
+        }
+        for (int i = 0; i < items.size(); i++) {
+
+            Item it = items.get(0);
+            items.remove(0);
+            CheckSet(items);
+            items.add(it);
+        }
+        for (int i = 0; i <items.size() ; i++) {
+
+            Item it = items.get(0);
+            items.remove(i);
+            delete(items);
+            items.add(it);
+        }
+
+    }
+
+    public List<Item> GetBestSet()
+    {
+        return bestItems2;
+    }
+}
